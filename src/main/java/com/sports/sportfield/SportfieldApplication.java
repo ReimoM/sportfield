b21e1192e3c9e03f3efbe6ee1e4cb60fe55f3de9
ReimoM <reimosalo@gmail.com>
2022-04-26 13:36:07 +0300

    Update

---
+++ src/main/java/com/sports/sportfield/SportfieldApplication.java
@@ -0,0 +1,13 @@
+package com.sports.sportfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportfieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportfieldApplication.class, args);
+	}
+
+}
