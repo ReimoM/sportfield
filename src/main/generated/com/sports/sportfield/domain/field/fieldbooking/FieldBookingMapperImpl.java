893b509c6f0238ca93097398c2309583daff7442
ReimoM <reimosalo@gmail.com>
2022-05-08 12:55:41 +0300

    user update

--- src/main/generated/com/sports/sportfield/domain/field/fieldbooking/FieldBookingMapperImpl.java
+++ src/main/generated/com/sports/sportfield/domain/field/fieldbooking/FieldBookingMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-08T12:08:57+0300",
+    date = "2022-05-08T12:16:35+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 16.0.2 (Amazon.com Inc.)"
 )
 @Component
